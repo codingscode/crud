076f437e5b8b8edfd188688b23255705fc39dbc0
Heronnes Pereira <heronnestg@hotmail.com>
2019-12-09 21:20:58 -0300

    parte 8

--- frontend/src/components/usuario/CrudUsuario.jsx
+++ frontend/src/components/usuario/CrudUsuario.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import Principal from '../template/Principal'
-
+import axios from 'axios'
 
 const headerProps = {
    icon: 'users',
@@ -8,7 +8,36 @@ const headerProps = {
    subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir!'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+const inicialState = {
+   usuario: {nome: '', email: ''},
+   lista: []
+ }
+
 export default class CrudUsuario extends Component {
+   state = {...inicialState}
+
+   clear() {
+     this.setState({user: inicialState.usuario})
+   }
+
+   save() {
+     const usuario = this.state.usuario
+     const metodo = usuario.id ? 'put' : 'post'
+     const url = usuario.id ? `${baseUrl}/${usuario.id}` : baseUrl
+     axios[metodo](url, usuario)
+        .then(resp => {
+          const lista = this.getUpdatedList(resp.data)
+          this.setState({usuario: inicialState.usuario, lista})
+        })
+   }
+
+   getUpdatedList(usuario) {
+      const lista = this.state.lista.filter(u => u.id !== usuario.id)
+      lista.unshift(usuario)
+      return lista
+   }
+
    render() {
      return (
      <Principal {...headerProps}>
